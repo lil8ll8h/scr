50893efeb9bd80a9bc537415838d247ad1724420
lil8ll8h <kenkneki77722@gmail.com>
2025-12-01 14:28:30 +0500

    Update guts n blackpowder.lua

--- guts n blackpowder.lua
+++ guts n blackpowder.lua
@@ -1,9 +1,9 @@
      local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Guts & Bluupowder",
+   Name = "🇷🇺Zov Hub💤 | Guts & Blackpowder",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Guts & Bluupowder Hub",
+   LoadingTitle = "🇷🇺Zov Hub💤",
    LoadingSubtitle = "by nobody",
    ShowText = "G&B", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
@@ -1309,3 +1309,4 @@ return {
     cleanup = cleanupAimbot
 
 }
+
